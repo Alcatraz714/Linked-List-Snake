452fa761c9ec5e281fa6d8b1507f037acd70ff5d
Rishi Saxena <rishi.saxena@gamestacy.com>
2024-11-27 14:32:29 +0530

    Added Level view and Rectangleview UI element

---
+++ Linked-List-Snake/include/Level/LevelController.h
@@ -0,0 +1,14 @@
+#pragma once
+namespace Level
+{
+    class LevelController
+    {
+    public:
+        LevelController();
+        ~LevelController();
+
+        void initialize();
+        void update();
+        void render();
+    };
+}
