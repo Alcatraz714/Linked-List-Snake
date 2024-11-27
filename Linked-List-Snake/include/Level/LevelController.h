5e08385dfb9866a15de6b59f9f8d22e37ee73539
Rishi Saxena <rishi.saxena@gamestacy.com>
2024-11-27 14:39:46 +0530

    Added Level Model and Controller code

--- Linked-List-Snake/include/Level/LevelController.h
+++ Linked-List-Snake/include/Level/LevelController.h
@@ -1,8 +1,17 @@
 #pragma once
+#include <vector>
+#include "LevelModel.h"
+
 namespace Level
 {
+    class LevelView;
+
     class LevelController
     {
+    private:
+        LevelModel* level_model;
+        LevelView* level_view;
+
     public:
         LevelController();
         ~LevelController();
@@ -10,5 +19,8 @@ namespace Level
         void initialize();
         void update();
         void render();
+
+        float getCellWidth();
+        float getCellHeight();
     };
 }
