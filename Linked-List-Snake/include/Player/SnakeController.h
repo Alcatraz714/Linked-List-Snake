4f29af00a4cab6d44b804efb48bcda4a8798d178
Rishi Saxena <rishi.saxena@gamestacy.com>
2024-11-28 14:13:01 +0530

    Added snake handle methods and enum for snake

--- Linked-List-Snake/include/Player/SnakeController.h
+++ Linked-List-Snake/include/Player/SnakeController.h
@@ -2,16 +2,43 @@
 
 namespace Player
 {
+	enum class SnakeState
+	{
+		ALIVE,
+		DEAD,
+	};
+
 	class SnakeController
 	{
 	private:
 
+		//const vars and handles
+		const int initial_snake_length = 10;
+		SnakeState current_snake_state;
+
+		//functions
+		void processPlayerInput();
+		void updateSnakeDirection();
+		void moveSnake();
+		void processSnakeCollision();
+		void handleRestart();
+		void reset();
+		void destroy();
+
 	public:
+
 		SnakeController();
 		~SnakeController();
 
 		void initialize();
 		void update();
 		void render();
+
+		//Spawn
+		void spawnSnake();
+		void respawnSnake();
+		//Getter and Setter
+		void setSnakeState(SnakeState state);
+		SnakeState getSnakeState();
 	};
 }
